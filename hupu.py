d59a40974892e7f566985bcaf710c1caa885a134
linsuk381 <64388997+linsuk381@users.noreply.github.com>
2022-06-21 16:10:59 +0800

    Update hupu.py

--- hupu.py
+++ hupu.py
@@ -17,7 +17,8 @@ def hupu():
         driver.find_element_by_xpath("//a[contains(text(),'点我签到~')]").click()
         time.sleep(1)
         driver.find_element_by_xpath("//button[contains(text(),'我要签到')]").click()
-        print("/html/body/div[@id='layui-layer4']/div[@class='layui-layer-content layui-layer-padding']")
+        if driver.find_elements_by_xpath("/html/body/div[@id='layui-layer4']/div[@class='layui-layer-content layui-layer-padding']") != []:
+            print("success")
     except:
         raise
     finally:

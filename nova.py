60d33a76d0200a1d188a75beb6bcde53c2a592f4
linsuk381 <64388997+linsuk381@users.noreply.github.com>
2022-06-21 18:28:28 +0800

    Create nova.py

---
+++ nova.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+from util import *
+import time
+
+username = sys.argv[1] # 登录账号
+password = sys.argv[2] # 登录密码
+
+@retry(stop_max_attempt_number=5)
+def hupu():
+    try:
+        driver = get_web_driver()
+        driver.get("https://www.q88q.cyou/auth/login")
+        driver.find_element_by_xpath("//input[@id='email']").send_keys(username)
+        driver.find_element_by_xpath("//input[@id='passwd']").send_keys(password)
+        driver.find_element_by_xpath("//button[@id='login']").click()
+        time.sleep(1)
+        driver.find_element_by_xpath("//button[@id='checkin']").click()
+        printf("成功！")
+    except:
+        raise
+    finally:
+        driver.quit()
+
+if __name__ == '__main__':
+    hupu()
